d18f56290ec8dbcab06098d5f98b02baf7afe1f3
Marcus Lönneborg <marcuslonneborg@gmail.com>
2015-09-06 19:54:20 +0200

    Test project

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Deck 
+{
+    public void Shuffle()
+    {
+
+    }
+    public void PrintDeck()
+    {
+
+    }
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.ReadKey();
+        }
+    }
+
